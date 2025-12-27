4e9bcb7a46700328f2d9cbe70967db45b11dc2c7
juankalebe <ojuankalebe@gmail.com>
2025-12-27 00:21:48 -0300

    Aula input

---
+++ aula-input.py
@@ -0,0 +1,21 @@
+# Crie um programa que peça para ler a idade de uma pessoa.
+# Tente imprimir a idade lida, na tela, sem utilizar variáveis.
+
+#print(input("Idade: ")) 
+#print("Ainda pode fazer assim",input("Idade: ")) 
+
+# Agora introduza uma variável para armazenar o dado e então imprima na tela
+
+#dado = input("Idade: ")
+#print("Usando uma variavel: ",dado)
+
+# DESAFIO
+# Crie um programa que peça ao usuário que insira seu nome 
+# seu telefone e seu numero de identidade.
+# Ao final, imprima as informações dadas pelo usuário
+
+nome = input("Nome: ")
+telefone = input("Telefone: ")
+identidade = input("Identidade: ")
+
+print("Nome: ",nome,"\nTelefone: ",telefone,"\nIdentidade: ",identidade)
